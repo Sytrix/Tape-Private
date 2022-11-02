871c97f09e9bcbc22aa8cbbe4a9b91840e5c9e05
llameiq <77410796+eLeCtRaDoMiNuS@users.noreply.github.com>
2022-11-02 20:45:33 +0300

    Update whitelistfortapeprivet.lua

--- whitelistfortapeprivet.lua
+++ whitelistfortapeprivet.lua
@@ -4,9 +4,9 @@ return  {
 	[""] 
 		= {"SPECIAL GUY",Color3.new(255, 8, 8)}, 
 	[""] 
-		= {"Pogi",Color3.new(255, 8, 8)}, 
+		= {"Tape",Color3.new(255, 8, 8)}, 
 	[""] 
-		= {"Pogi",Color3.new(255, 8, 8)}, 
+		= {"Tape",Color3.new(255, 8, 8)}, 
 	[""] 
 	        = {"AC MOD",Color3.new(255, 8, 8)}, 
 	["03b172e963326d73baaad194df2d2d46ad9995b9146a28b0d07b383de1c739ba93ce3ece7fea41556bf2c6d4634cdc9adff562036049201e57eddba4c4d380ea"]
