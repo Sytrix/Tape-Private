e764ce34f58875d041c9514ecb1bdb9abba38b11
llameiq <77410796+eLeCtRaDoMiNuS@users.noreply.github.com>
2022-11-02 20:12:24 +0300

    Create whitelistfortapeprivet.lua

---
+++ whitelistfortapeprivet.lua
@@ -0,0 +1,14 @@
+return  {
+        [""]
+		= {"SPECIAL GUY",Color3.new(255, 8, 8)},
+	[""] 
+		= {"SPECIAL GUY",Color3.new(255, 8, 8)}, 
+	[""] 
+		= {"Pogi",Color3.new(255, 8, 8)}, 
+	[""] 
+		= {"Pogi",Color3.new(255, 8, 8)}, 
+	[""] 
+	        = {"AC MOD",Color3.new(255, 8, 8)}, 
+	[""]
+	        = {"TAPE OWNER",Color3.new(165, 10, 204)}, --NPM
+}
