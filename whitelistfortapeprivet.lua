897853cf6036434fac71c5be748ff86afa1582ca
llameiq <77410796+eLeCtRaDoMiNuS@users.noreply.github.com>
2022-11-02 20:40:24 +0300

    Update whitelistfortapeprivet.lua

--- whitelistfortapeprivet.lua
+++ whitelistfortapeprivet.lua
@@ -9,6 +9,6 @@ return  {
 		= {"Pogi",Color3.new(255, 8, 8)}, 
 	[""] 
 	        = {"AC MOD",Color3.new(255, 8, 8)}, 
-	[""]
+	["03b172e963326d73baaad194df2d2d46ad9995b9146a28b0d07b383de1c739ba93ce3ece7fea41556bf2c6d4634cdc9adff562036049201e57eddba4c4d380ea"]
 	        = {"TAPE OWNER",Color3.new(165, 10, 204)}, --NPM
 }
